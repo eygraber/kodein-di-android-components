8488d36d06d30c8257fa68908e48419ae647380a
Eliezer Graber <eygraber@gmail.com>
2019-08-04 17:01:57 -0400

    Provide a scope for fragment views

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Dependency
 Configure this dependency:
 
 ```kotlin
-implementation "com.eygraber:kodein-android-components:1.0.1"
+implementation "com.eygraber:kodein-android-components:1.1.0"
 ```
--- library/src/main/java/com/eygraber/kodein/kodein.kt
+++ library/src/main/java/com/eygraber/kodein/kodein.kt
@@ -3,50 +3,72 @@ package com.eygraber.kodein
 import android.app.Activity
 import android.app.Service
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleService
-import androidx.lifecycle.OnLifecycleEvent
 import org.kodein.di.Kodein
-import org.kodein.di.bindings.NoArgSimpleBindingKodein
-import org.kodein.di.bindings.Scope
-import org.kodein.di.bindings.ScopeRegistry
-import org.kodein.di.bindings.StandardScopeRegistry
-import org.kodein.di.bindings.WeakContextScope
+import org.kodein.di.bindings.*
 import org.kodein.di.erased.scoped
 import org.kodein.di.erased.singleton
 import org.kodein.di.internal.synchronizedIfNull
 import org.kodein.di.generic.scoped as genericallyScoped
 import org.kodein.di.generic.singleton as genericSingleton
 
-object AndroidScope : Scope<Any> {
-  private val newRegistry = ::StandardScopeRegistry
-  private val map = HashMap<LifecycleOwner, ScopeRegistry>()
+@PublishedApi internal object AndroidScope : Scope<Any> {
+    private val newRegistry = ::StandardScopeRegistry
+    private val map = HashMap<LifecycleOwner, ScopeRegistry>()
 
-  override fun getRegistry(context: Any): ScopeRegistry {
-    (context as? LifecycleOwner)
-        ?: throw IllegalStateException("The context of an AndroidScope must be a LifecycleOwner")
+    override fun getRegistry(context: Any): ScopeRegistry {
+        (context as? LifecycleOwner)
+            ?: throw IllegalStateException("The context of an AndroidScope must be a LifecycleOwner")
 
-    return synchronizedIfNull(
-        lock = map,
-        predicate = { map[context] },
-        ifNotNull = { it },
-        ifNull = {
-          val registry = newRegistry()
-          map[context] = registry
-          context.lifecycle.addObserver(object : LifecycleObserver {
-            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-            fun onDestroy() {
-              context.lifecycle.removeObserver(this)
-              registry.clear()
-              map.remove(context)
+        return synchronizedIfNull(
+            lock = map,
+            predicate = { map[context] },
+            ifNotNull = { it },
+            ifNull = {
+                val registry = newRegistry()
+                map[context] = registry
+                context.lifecycle.addObserver(
+                    object : DefaultLifecycleObserver {
+                    override fun onDestroy(owner: LifecycleOwner) {
+                        context.lifecycle.removeObserver(this)
+                        registry.clear()
+                        map.remove(context)
+                    }
+                }
+                )
+                registry
             }
-          })
-          registry
-        }
-    )
-  }
+        )
+    }
+}
+
+@PublishedApi internal object FragmentViewScope : Scope<Fragment> {
+    private val newRegistry = ::StandardScopeRegistry
+    private val map = HashMap<LifecycleOwner, ScopeRegistry>()
+
+    override fun getRegistry(context: Fragment): ScopeRegistry {
+        return synchronizedIfNull(
+            lock = map,
+            predicate = { map[context] },
+            ifNotNull = { it },
+            ifNull = {
+                val registry = newRegistry()
+                map[context] = registry
+                context.viewLifecycleOwner.lifecycle.addObserver(
+                    object : DefaultLifecycleObserver {
+                        override fun onDestroy(owner: LifecycleOwner) {
+                            context.lifecycle.removeObserver(this)
+                            registry.clear()
+                            map.remove(context)
+                        }
+                    }
+                )
+                registry
+            }
+        )
+    }
 }
 
 inline fun <reified T : Any> Kodein.Builder.activitySingleton(
@@ -57,13 +79,21 @@ inline fun <reified T : Any> Kodein.Builder.genericActivitySingleton(
     noinline creator: NoArgSimpleBindingKodein<Activity>.() -> T
 ) = genericallyScoped<Activity>(AndroidScope).genericSingleton(creator = creator)
 
-inline fun <reified T : Any> Kodein.Builder.supportFragmentSingleton(
+inline fun <reified T : Any> Kodein.Builder.fragmentSingleton(
     noinline creator: NoArgSimpleBindingKodein<Fragment>.() -> T
 ) = scoped<Fragment>(AndroidScope).singleton(creator = creator)
 
-inline fun <reified T : Any> Kodein.Builder.genericSupportSingleton(
+inline fun <reified T : Any> Kodein.Builder.fragmentViewSingleton(
+    noinline creator: NoArgSimpleBindingKodein<Fragment>.() -> T
+) = scoped(FragmentViewScope).singleton(creator = creator)
+
+inline fun <reified T : Any> Kodein.Builder.genericFragmentSingleton(
     noinline creator: NoArgSimpleBindingKodein<Fragment>.() -> T
 ) = genericallyScoped<Fragment>(AndroidScope).genericSingleton(creator = creator)
+
+inline fun <reified T : Any> Kodein.Builder.genericFragmentViewSingleton(
+    noinline creator: NoArgSimpleBindingKodein<Fragment>.() -> T
+) = genericallyScoped(FragmentViewScope).genericSingleton(creator = creator)
 
 inline fun <reified T : Any> Kodein.Builder.serviceSingleton(
     noinline creator: NoArgSimpleBindingKodein<Service>.() -> T
